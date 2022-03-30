ad2398be0a315723a5957ba2d5086827bbd92e0d
Mamun <a.a.mamunbu@gmail.com>
2022-03-30 15:39:42 +0600

    Namespace and LEGB rules

---
+++ Python/decorator.py
@@ -0,0 +1,21 @@
+"""
+Defination :
+
+Python has an interesting feature called decorators since it allows programmers to modify the behaviour of function or class.
+Decorators allow us to wrap another function in order to extend the behaviour of the wrapped function, without permanently modifying it
+
+This is also called metaprogramming because a part of the program tries to modify another part of the program at compile time.
+
+
+Related Topic you should know:
+* Closure 
+* In python Everything is Object
+* Function are first class object / First class Fuction
+
+
+Resource :
+
+https://www.geeksforgeeks.org/decorators-in-python/
+https://youtu.be/FsAPt_9Bf3U
+
+"""
---
+++ Python/nameSpace-variableScope-LEGB-Rules.py
@@ -0,0 +1,65 @@
+"""
+Namespace :
+
+A namespace is a system that has a unique name for each and every object in Python. 
+An object might be a variable or a method. Python itself maintains a namespace in the form of a Python dictionary.
+
+Type of Namespace :
+
+When Python interpreter runs solely without any user-defined modules, methods, classes, etc.
+Some functions like print(), id() are always present, these are built-in namespaces.
+When a user creates a module, a global namespace gets created, later the creation of local functions creates the local namespace.
+The built-in namespace encompasses the global namespace and the global namespace encompasses the local namespace.
+
+* Builtin Namespace
+* Global Namespace
+* Local Namespace 
+
+Check the namespace from the scope using DIR() builtin function 
+
+
+LEGB Scope :
+* Local
+* Global
+* Enclosed
+* Builtin 
+
+* global 
+* nonlocal 
+
+Note : * To chage the global variable value in local scope use global keyword with local scope variable
+	   * To chage the enclosed variable value in local scope use nonlocal keyword with local scope variable
+
+
+
+Resource :
+
+Namespace : 
+	https://www.geeksforgeeks.org/namespaces-and-scope-in-python/
+
+
+"""
+
+
+# LEGB EXAMPLE 
+
+
+
+
+x = 0 # Global scope
+print(x) # Built-in Scope
+def outer():
+	global x
+	x = x + 3
+	y = 5 # local scope
+	print(x)
+	def inner():
+		nonlocal y
+		y = y + 10 # enclosed scope
+		print(y)
+	inner()
+outer()
+
+
+
+
