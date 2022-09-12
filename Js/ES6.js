f16be9c78d28095b8e85404f99cb7b09514f4128
Mamun <a.a.mamunbu@gmail.com>
2022-09-12 08:33:06 +0600

    leetcode 136

---
+++ Js/ES6.js
@@ -0,0 +1,10 @@
+const obj = {
+    name : "hasan",
+    age : 30,
+    info(){
+        return `Information name ${this.name} and age ${this.age}`
+    }
+}
+
+console.log(obj.info())
+
---
+++ Problem-Solving/LeetCode/136. Single Number.py
@@ -0,0 +1,5 @@
+nums = [4,1,2,1,2]
+# Output: 4
+
+
+print(nums.count(1))
