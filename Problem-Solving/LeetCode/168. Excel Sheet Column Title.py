25937cfb251b267cf382eeae526254c47fa4b86e
Mamun <a.a.mamunbu@gmail.com>
2023-01-05 09:38:46 +0600

    leetcode - 2023 - day - 5

--- Problem-Solving/LeetCode/168. Excel Sheet Column Title.py
+++ Problem-Solving/LeetCode/168. Excel Sheet Column Title.py
@@ -1,7 +1,22 @@
-# print(chr(65,90))
+# # print(chr(65,90))
 
-result = chr(ord('A') + (1600 - 1) % 26) + ''
+# result = chr(ord('A') + (1600 - 1) % 26) + ''
+
+# print(result)
+
+
+# Input: columnNumber = 28
+# Output: "AB"
+
+
+columnNumber = 28
+
+result = ''
+
+while columnNumber>0:
+	
+	result = chr(ord('A')+(columnNumber-1)%26 )+ result
+
+	columnNumber = (columnNumber-1) // 26
 
 print(result)
-
-
