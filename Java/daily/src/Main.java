82f590acfafe8f29edc69a6a9503f2e3c9b2692f
Mamun <a.a.mamunbu@gmail.com>
2023-09-03 15:49:02 +0600

    java day one

---
+++ Java/daily/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Primitive data types
+        int viewCount = 123_123_123; // in every three digit we can use _ for clear visibility
+        long viewCount2 = 3_123_123_123L;
+        float price = 10.30F;
+        char letter ='A';
+        String name = "Mamun";
+        boolean isEligible = true;
+
+//        Reference Data types
+        Date now = new Date();
+        System.out.println(now);
+
+
+
+
+    }
+}
