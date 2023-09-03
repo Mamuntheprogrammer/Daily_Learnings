ea89dbbc2c29635f42e8e2e944217cba670b7848
Mamun <a.a.mamunbu@gmail.com>
2023-09-03 21:06:06 +0600

    office

--- Java/daily/src/Main.java
+++ Java/daily/src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
@@ -8,12 +10,36 @@ public class Main {
         long viewCount2 = 3_123_123_123L;
         float price = 10.30F;
         char letter ='A';
-        String name = "Mamun";
+
         boolean isEligible = true;
 
 //        Reference Data types
         Date now = new Date();
         System.out.println(now);
+
+        Point point1 = new Point(2,3);
+        Point point2 = point1;
+        point2.x = 10;
+        System.out.println(point1);
+
+//         String in Java ( String are reference type in java
+//        String is immutable in Java
+
+        String name = new String("Mamun");
+//        or
+        String name2 = "Mamun2";
+        System.out.println(name + " " +name2);
+
+//        Escape Sequences
+        System.out.println("Hello \"Mamun\" ");
+
+//        Arrays
+
+        int[] numbers = new int[5];
+        int[] numbers2 = {1,2,3};
+        System.out.println(Arrays.toString(numbers));
+        System.out.println(Arrays.toString(numbers2));
+
 
 
 
