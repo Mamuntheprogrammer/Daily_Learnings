67428526db6ad9d162a8aebe6de7d4088968ae38
Mamun <a.a.mamunbu@gmail.com>
2023-09-06 23:29:23 +0600

    offday home

--- Java/daily/src/OOP.java
+++ Java/daily/src/OOP.java
@@ -3,4 +3,10 @@ public class OOP {
     public void setStr(String text){
         this.text = text;
     }
+
+//     note that java memory allocation
+//    HEAP = store objects
+//    STACK = primitive types or short lived variables
+
+
 }
