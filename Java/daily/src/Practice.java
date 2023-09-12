d209b1434803a1ec474c5c185235ee4935d6b798
Mamun <a.a.mamunbu@gmail.com>
2023-09-12 08:26:03 +0600

    OOP concept in java

--- Java/daily/src/Practice.java
+++ Java/daily/src/Practice.java
@@ -1,14 +1,11 @@
-import java.io.Console;
-import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.Scanner;
+
 
 public class Practice {
-    public static void main(String[] args) {
-       int i[] = {12,2,53,4};
-        Arrays.sort(i);
-        System.out.println(Arrays.toString(i));
+    private String name;
+    private double salary;
 
-        
+public Practice{
+    
+}
 
-}}
+}
