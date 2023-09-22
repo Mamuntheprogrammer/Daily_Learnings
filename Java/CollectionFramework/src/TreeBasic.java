a4976b77e4cf7170b847cab7581c1d4fd7f55f71
Mamun <a.a.mamunbu@gmail.com>
2023-09-22 22:54:40 +0600

    ok

--- Java/CollectionFramework/src/TreeBasic.java
+++ Java/CollectionFramework/src/TreeBasic.java
@@ -1,3 +1,14 @@
 public class TreeBasic {
-    
+//    root node
+//    parent node
+//    right child, left child
+//    sibling
+//    leaf
+//    ancester
+//    level
+//    subtree ( right / left subtree)
+//    build tree preorder
+//    null node = -1
+
+
 }
