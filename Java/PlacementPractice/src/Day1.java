2843d5b80d54f6c5e2dc22eb549fe5af64f81c38
Mamun <a.a.mamunbu@gmail.com>
2023-09-24 15:35:51 +0600

    day1

---
+++ Java/PlacementPractice/src/Day1.java
@@ -0,0 +1,14 @@
+public class Day1 {
+
+//    how javacode execute
+// Java Compilation process :  source code --> Java compiler (Janvac) in JDK --> Bytecode
+// Java execution process   : Bytecode --> (JVM from java JRE) --> native code (Machine code / binary code )
+
+    
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
