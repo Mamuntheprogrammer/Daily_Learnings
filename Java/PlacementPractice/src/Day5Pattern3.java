4b8e802d53b19dbd7ef3b12c9764780dd21c1220
Mamun <a.a.mamunbu@gmail.com>
2023-09-26 15:16:21 +0600

    pattern

--- Java/PlacementPractice/src/Day5Pattern3.java
+++ Java/PlacementPractice/src/Day5Pattern3.java
@@ -1,5 +1,12 @@
 public class Day5Pattern3 {
     public static void main(String[] args) {
-        
+//        0 - 1 TRIANGLE
+
+        for (int i=1;i<8;i++){
+            for (int j =1;j<i;j++){
+                if ((i+j)%2==0) System.out.print(1);else System.out.print(0);
+            }
+            System.out.println();
+        }
     }
 }
