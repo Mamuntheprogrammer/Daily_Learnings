599cfa7aacf697d4751e64f6b7ce8c619e7f53b1
Mamun <a.a.mamunbu@gmail.com>
2023-10-17 14:41:10 +0600

    k

--- Java/PlacementPractice/src/Array1.java
+++ Java/PlacementPractice/src/Array1.java
@@ -1,17 +1,26 @@
+import java.util.Scanner;
+
 public class Array1 {
 
 
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int row = scan.nextInt();
+        int col = scan.nextInt();
 
-        int marks[] = new int[3];
+  int[][] mat = new int[row][col];
 
-        marks[0] = 1;
-
-        for(int i=0;i<marks.length;i++){
-            System.out.println(marks[i]);
-
+  for(int i =0;i<row;i++){
+      for (int j = 0;j<col;j++){
+          mat[i][j] = scan.nextInt();
+      }
+  }
+        for(int i =0;i<row;i++){
+            for (int j = 0;j<col;j++){
+                System.out.print(mat[i][j]);
+            }
+            System.out.println();
         }
-
     }
 }
