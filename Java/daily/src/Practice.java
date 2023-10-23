fd27fbbb53647d516496c42efa1504bd0013b6b4
Mamun <a.a.mamunbu@gmail.com>
2023-10-23 16:04:35 +0600

    reverse a string

--- Java/daily/src/Practice.java
+++ Java/daily/src/Practice.java
@@ -1,11 +1,21 @@
+public class Practice{
+    public static void main(String[] arg){
+        // Reverse a strint
+        StringBuilder sb = new StringBuilder("I Hate working here");
+
+        for(int i=0; i<sb.length()/2;i++){
+            int front = i;
+            int back = sb.length()-1-i;
+
+            char fronCha = sb.charAt(front);
+            char backCha = sb.charAt(back);
+
+            sb.setCharAt(front, backCha);
+            sb.setCharAt(back, fronCha);
+        }
+
+        System.out.println(sb);
 
 
-public class Practice {
-    private String name;
-    private double salary;
-
-public Practice{
-    
-}
-
+    }
 }
