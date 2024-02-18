e326a155b8d85b7f94ec02b30be0ac68514fb966
Mamun <a.a.mamunbu@gmail.com>
2024-02-18 16:28:20 +0600

    off-23

--- Python/Practice.py
+++ Python/Practice.py
@@ -1,3 +1,202 @@
+def repeat(num_times):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(num_times):
+                result = func(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+@repeat(num_times=3)
+def greet(name):
+    print(f"Hello, {name}!")
+
+greet("Alice")
+
+
+
+
+
+
+
+
+
+
+
+
+# # Decorator 
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("something will happen")
+#         func()
+#         print("decorator part done")
+#     return wrapper
+
+# @my_decorator
+# def name():
+#     print("I called from decorator")
+# name()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def make_greetings(names):
+#     funcs = []
+#     for name in names:
+#         funcs.append(lambda name=name: print('Hello', name))
+#     return funcs
+# # Try it
+# a, b, c = make_greetings(['Guido', 'Ada', 'Margaret'])
+# a() # Prints 'Hello Guido'
+# b() # Prints 'Hello Ada'
+# c() # Prints 'Hello Margaret'
+
+# return funcs: After all names have been processed, the list funcs, containing the lambda functions, is returned from the make_greetings function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # High order functions can be
+# # passed as arguments to other functions, placed in data structures, and returned by a
+# # function as a result.
+# import time
+# def after(seconds, func):
+#     time.sleep(seconds)
+#     func()
+# # Example usage
+# def greeting():
+#     print('Hello World')
+# after(10, greeting)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # One of the main uses of lambda is to define small callback functions. For example, you
+# # may see it used with built-in operations such as sorted(). For example:
+
+# words=['xyx','yyxj','x']
+
+# # Sort a list of words by the number of unique letters
+# result = sorted(words, key=lambda word: len(set(word)))
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # To return multiple value return a tuple 
+# def retrunMv(*arg):
+#     a,b,*c=arg
+#     return (a,b,c)
+
+# print(retrunMv(1,23,4,5))
+
+
+
+
+
+
+
+
+
+
+
+
+# The args is in dictinary form
+# def one(**args):
+#     print(args)
+
+# one(mamun=25,hasan=30)
+
+
+
+
+
+
+
+
+
+
+
+# raise SystemExit() # Exit with no error message
+# raise SystemExit("Something is wrong") # Exit with error
+
+
+# import atexit
+
+
+
+
+
+
+# Exceptions
+
+# try:
+#     div = 10/2
+# except Exception as e:
+#     print(f"From except block , The error is {e}")
+# finally:
+#     print("Done")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #Done
 
 # pairs = [('IBM', 125), ('ACME', 50), ('PHP', 40)]
